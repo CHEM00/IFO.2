5946d1544fff66c3ba496247b561739776e1ccf0
Chem00 <zs19017404@estudiantes.uv.mx>
2024-10-24 01:18:48 -0600

    Agregar pantalla de inicio, facturas y estilos de cada una de las pantallas

--- IntekelFinancer/routes/web.php
+++ IntekelFinancer/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RutasController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/registrarse', [RutasController::class, 'Registro']) -> name('Registre');
+
+Route::get('/login', [RutasController::class, 'Login']) -> name('Login');
+
+Route::get('/inicio', [RutasController::class, 'Home']) -> name('Inicio');
+
+Route::get('/facturas', function () {
+    return view ('innvoice.pending-innvoice');
+}) -> name('pending-innvoice');
